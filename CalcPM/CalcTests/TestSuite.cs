88a1fe1178cad896ea2cda92d8a86e007ffea289
Vova <vovafalkon@gmail.com>
2019-11-13 22:47:05 +0200

    Add xunit tests

---
+++ CalcPM/CalcTests/TestSuite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using Math = CalcFunction.Math;
+
+namespace CalcTests
+{
+
+    public class MathTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //arrange
+            var firstNumber = 12;
+            var secondNumber = 52;
+            //act
+            var res = Math.Add(firstNumber, secondNumber);
+            //assert
+            Assert.Equal(firstNumber+secondNumber,res);
+        }
+    }
+}
---
+++ CalcPM/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+using Math = CalcFunction.Math;
+
+namespace CalcTests
+{
+
+    public class MathTest
+    {
+        [Fact]
+        //[Fact(Skip = "Ignored this test")]// можна передати в параметри маркер типу ігнор і вказати опис
+        public void AddTest()
+        {
+            //arrange
+            var firstNumber = 12;
+            var secondNumber = 52;//ініціалізація роботи
+            //act
+            var res = Math.Add(firstNumber, secondNumber);// сама дія
+            //assert
+            Assert.Equal(firstNumber + secondNumber, res);// перевірка результату
+
+        }
+        [Theory]
+        [InlineData(12,52)]//можна винести ініціалізацію даних
+        [InlineData(32,62)]//test case
+        public void AddTestTheory(int firstNumber, int secondNumber)
+        {
+            //act
+            var res = Math.Add(firstNumber, secondNumber);
+            //assert
+            Assert.Equal(firstNumber + secondNumber, res);
+        }
+    }
+}
