ec36eb5ec37d407dec184cbdc3f697f6cd432e4e
KaiumovAleksandr <kayumovalexandr77@gmail.com>
2019-11-15 12:59:34 +0200

    Created CalcExceptionLibrary

---
+++ CalcPM/CalcExceptionLibrary/CalcException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcExceptionLibrary
+{
+    public class CalcException : Exception
+    {
+        public int ErrorPos { get; }
+        public int ErrorCode { get; }
+        public CalcException(int error_code, int error_index)
+        {
+            ErrorPos = error_index + 1;
+            ErrorCode = error_code;
+        }
+        public CalcException(int error_code) { ErrorCode = error_code; }
+    }
+}
